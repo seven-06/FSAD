77703f5a33b2e485511249e4c3f0dde33be056ef
Nikhil Yadav Genega <ni.khil40806@gmail.com>
2026-01-05 13:48:07 +0530

    Added SKILL - 1 folder with all files

---
+++ SKILL - 1/MyMavenApp/src/main/java/com/maven/Maven.java
@@ -0,0 +1,11 @@
+package com.maven;
+
+public class Maven {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is My Maven Project");
+
+	}
+
+}
